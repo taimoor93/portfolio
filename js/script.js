3469d604710ad3c9f424a2f20640f7a2ede7bccc
Taimoor Imran Baig <taimoor.imran@kalsoft.com>
2023-05-26 15:23:29 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".multiple_text", {
+    strings: ["Software Developer", "Software Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+
